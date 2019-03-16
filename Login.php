8c60ae50be5acb81d5916237929db129b5d3a760
arisavaich <arisa.va@live.ku.th>
2019-03-16 15:16:59 +0700

    Edit Login File V2

--- Login.php
+++ Login.php
@@ -1,141 +1,20 @@
-
-<?php
-if(!empty($_POST["register-user"])) {
-	/* Form Required Field Validation */
-	foreach($_POST as $key=>$value) {
-		if(empty($_POST[$key])) {
-		$error_message = "All Fields are required";
-		break;
-		}
-	}
-	/* Password Matching Validation */
-	if($_POST['password'] != $_POST['confirm_password']){ 
-	$error_message = 'Passwords should be same<br>'; 
-	}
-
-	/* Email Validation  Email */ 
-	if(!isset($error_message)) {
-		if (!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
-		$error_message = "Invalid Email Address";
-		}
-	}
-
-
-
-	/* Validation to check if Terms and Conditions are accepted */
-	if(!isset($error_message)) {
-		if(!isset($_POST["terms"])) {
-		$error_message = "Accept Terms and Conditions to Register";
-		}
-	}
-
-	if(!isset($error_message)) {
-		require_once("dbcontroller.php");
-		$db_handle = new DBController();
-		$query = "INSERT INTO registered_users (user_name, password, email, mobile) VALUES
-		('" . $_POST["userName"] . "', '" . $_POST["userPassword"] . "', '" . $_POST["userEmail"] . "','" . $_POST["mobile"] . "')";
-		$result = $db_handle->insertQuery($query);
-		if(!empty($result)) {
-			$error_message = "";
-			$success_message = "Register Successfully";
-			unset($_POST);
-		} else {
-			$error_message = Already Registered
-		}
-	}
-}
-?>
-<html>
-<head>
-<title> Arisa' Registration Form</title>
-<style>
-body{
-	width:610px;
-	font-family:calibri;
-}
-.error-message {
-	padding: 7px 10px;
-	background: #fff1f2;
-	border: #ffd5da 1px solid;
-	color: #d6001c;
-	border-radius: 4px;
-}
-.success-message {
-	padding: 7px 10px;
-	background: #cae0c4;
-	border: #c3d0b5 1px solid;
-	color: #027506;
-	border-radius: 4px;
-}
-.demo-table {
-	background: #d9eeff;
-	width: 100%;
-	border-spacing: initial;
-	margin: 2px 0px;
-	word-break: break-word;
-	table-layout: auto;
-	line-height: 1.8em;
-	color: #333;
-	border-radius: 4px;
-	padding: 20px 40px;
-}
-.demo-table td {
-	padding: 15px 0px;
-}
-.demoInputBox {
-	padding: 10px 30px;
-	border: #a9a9a9 1px solid;
-	border-radius: 4px;
-}
-.btnRegister {
-	padding: 10px 30px;
-	background-color: #3367b2;
-	border: 0;
-	color: #FFF;
-	cursor: pointer;
-	border-radius: 4px;
-	margin-left: 10px;
-}
-</style>
-</head>
-<body>
-<form name="frmRegistration" method="post" action="">
-<table border="0" width="500" align="center" class="demo-table">
-<?php if(!empty($success_message)) { ?>	
-<div class="success-message"><?php if(isset($success_message)) echo $success_message; ?></div>
-<?php } ?>
-<?php if(!empty($error_message)) { ?>	
-<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
-<?php } ?>
-<tr>
-<td>User Name</td>
-<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" class="demoInputBox" name="password" value=""></td>
-</tr>
-<tr>
-<td>Confirm Password</td>
-<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-</tr>
-<tr>
-<td>Email</td>
-<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
-</tr>
-<tr>
-<tr>
-<td>Mobile</td>
-<td><input type="text" class="demoInputBox" name="userMobile" value="<?php if(isset($_POST['userMobile'])) echo $_POST['userMobile']; ?>"></td>
-</tr>
-<tr>
-
-
-
-<tr>
-<td colspan=2>
-<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
-</tr>
-</table>
+<form name="form1" method="post" action="check_login.php">
+  Login<br>
+  <table border="1" style="width: 300px">
+    <tbody>
+      <tr>
+        <td> E-mail</td>
+        <td>
+          <input name="txtUsername" type="text" id="txtUsername">
+        </td>
+      </tr>
+      <tr>
+        <td> &nbsp;Password</td>
+        <td><input name="txtPassword" type="password" id="txtPassword">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <input type="submit" name="Submit" value="Login">
 </form>
-</body></html>
